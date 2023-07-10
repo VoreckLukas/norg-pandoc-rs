bca2d6d2ca1a2141ed751b36f487d0dd469c7396
Voreck Lukas <voreck-lukas@tutanota.com>
2023-07-10 23:05:19 +0200

    Attached Modifiers

--- src/inline/mod.rs
+++ src/inline/mod.rs
@@ -4,6 +4,8 @@ use pandoc_ast::Inline;
 
 use crate::Meta;
 
+mod attached;
+
 pub fn parse(parse_meta: &mut Meta) -> LinkedList<Inline> {
     let mut inlines = match parse_meta.tree.node().kind() {
         "paragraph_segment" => {
@@ -13,6 +15,26 @@ pub fn parse(parse_meta: &mut Meta) -> LinkedList<Inline> {
                 LinkedList::new()
             }
         }
+
+        "bold" => LinkedList::from([attached::parse(parse_meta, attached::Type::Bold)]),
+
+        "italic" => LinkedList::from([attached::parse(parse_meta, attached::Type::Italic)]),
+
+        "underline" => LinkedList::from([attached::parse(parse_meta, attached::Type::Underline)]),
+
+        "strikethrough" => {
+            LinkedList::from([attached::parse(parse_meta, attached::Type::Strikethrough)])
+        }
+
+        "spoiler" => LinkedList::from([attached::parse(parse_meta, attached::Type::Spoiler)]),
+
+        "superscript" => {
+            LinkedList::from([attached::parse(parse_meta, attached::Type::Superscript)])
+        }
+
+        "subscript" => LinkedList::from([attached::parse(parse_meta, attached::Type::Subscript)]),
+
+        "verbatim" => LinkedList::from([attached::parse(parse_meta, attached::Type::Code)]),
 
         "_line_break" => LinkedList::from([Inline::SoftBreak]),
 
@@ -24,6 +46,8 @@ pub fn parse(parse_meta: &mut Meta) -> LinkedList<Inline> {
                 .unwrap()
                 .to_owned(),
         )]),
+
+        "_close" | "_open" => LinkedList::new(),
 
         "_space" => LinkedList::from([Inline::Space]),
 
