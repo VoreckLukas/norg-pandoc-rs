af9e0e35d28a697f87716c96a71d1698c52850ca
Voreck Lukas <voreck-lukas@tutanota.com>
2023-07-11 20:50:09 +0200

    File Location links

--- src/inline/link.rs
+++ src/inline/link.rs
@@ -3,29 +3,44 @@ use pandoc_ast::Inline;
 use crate::{inline, Meta};
 
 pub fn parse(parse_meta: &mut Meta) -> Inline {
-    let target = if parse_meta.tree.goto_first_child()
-        && parse_meta.tree.goto_first_child()
-        && parse_meta.tree.goto_next_sibling()
-    {
-        match parse_meta.tree.node().kind() {
-            "link_target_url" => {
-                if !parse_meta.tree.goto_next_sibling() {
-                    unreachable!()
-                }
-
-                parse_meta
-                    .tree
-                    .node()
-                    .utf8_text(parse_meta.source)
-                    .unwrap()
-                    .to_owned()
+    let (target, desc_len) =
+        if parse_meta.tree.goto_first_child() && parse_meta.tree.goto_first_child() {
+            while parse_meta.tree.node().kind() == "_begin" {
+                parse_meta.tree.goto_next_sibling();
             }
 
-            _ => todo!(),
-        }
-    } else {
-        unreachable!()
-    };
+            match parse_meta.tree.node().kind() {
+                "link_target_url" => {
+                    if !parse_meta.tree.goto_next_sibling() {
+                        unreachable!()
+                    }
+
+                    let url = parse_meta
+                        .tree
+                        .node()
+                        .utf8_text(parse_meta.source)
+                        .unwrap()
+                        .to_owned();
+                    let desc_len = url.len();
+                    (url, desc_len)
+                }
+
+                "link_file_text" => {
+                    let mut file = parse_meta
+                        .tree
+                        .node()
+                        .utf8_text(parse_meta.source)
+                        .unwrap()
+                        .to_owned();
+                    file.push_str(".norg");
+                    let desc_len = file.len() - 5;
+                    (file, desc_len)
+                }
+                _ => todo!(),
+            }
+        } else {
+            unreachable!()
+        };
 
     let description = if parse_meta.tree.goto_parent()
         && parse_meta.tree.goto_next_sibling()
@@ -33,7 +48,7 @@ pub fn parse(parse_meta: &mut Meta) -> Inline {
     {
         inline::parse(parse_meta).into_iter().collect()
     } else {
-        vec![Inline::Str(target.clone())]
+        vec![Inline::Str(target[..desc_len].to_owned())]
     };
     parse_meta.tree.goto_parent();
 
