1ff728d2aa695d2c803c203e3721e07a89c4189f
Voreck Lukas <voreck-lukas@tutanota.com>
2023-07-17 12:25:35 +0200

    Add Urgent Todo List item

--- lib/src/block/list.rs
+++ lib/src/block/list.rs
@@ -1,6 +1,6 @@
 use std::collections::LinkedList;
 
-use pandoc_ast::{Block, ListNumberDelim, ListNumberStyle};
+use pandoc_ast::{Block, Inline, ListNumberDelim, ListNumberStyle};
 
 use crate::{inline, Meta};
 
@@ -58,11 +58,39 @@ fn parse_list(parse_meta: &mut Meta) -> (LinkedList<Vec<Block>>, ListType, usize
             .unwrap()
     };
 
-    let mut content = if parse_meta.tree.goto_first_child()
-        && parse_meta.tree.goto_next_sibling()
-        && parse_meta.tree.goto_first_child()
-    {
-        vec![Block::Para(inline::parse(parse_meta).into_iter().collect())]
+    let mut content = if parse_meta.tree.goto_first_child() && parse_meta.tree.goto_next_sibling() {
+        let mut inlines = if parse_meta.tree.node().kind() == "detached_modifier_extension" {
+            if !parse_meta.tree.goto_first_child() || !parse_meta.tree.goto_next_sibling() {
+                unreachable!()
+            }
+            let item = if parse_meta.tree.node().kind() == "todo_item_urgent" {
+                LinkedList::from([
+                    Inline::Strong(vec![Inline::Span(
+                        (
+                            String::default(),
+                            vec!["todo".to_string(), "urgent".to_string()],
+                            vec![],
+                        ),
+                        vec![Inline::Str("(!)".to_string())],
+                    )]),
+                    Inline::Space,
+                ])
+            } else {
+                eprintln!("{}", parse_meta.tree.node().kind());
+                todo!()
+            };
+
+            parse_meta.tree.goto_parent();
+            parse_meta.tree.goto_next_sibling();
+            item
+        } else {
+            LinkedList::new()
+        };
+        if !parse_meta.tree.goto_first_child() {
+            unreachable!()
+        }
+        inlines.append(&mut inline::parse(parse_meta));
+        vec![Block::Para(inlines.into_iter().collect())]
     } else {
         unreachable!()
     };
