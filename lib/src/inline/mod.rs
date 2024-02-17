c1cc862d197e65cdc95543dfc13c984f117d49c2
Voreck Lukas <voreck-lukas@tutanota.com>
2024-02-17 12:35:22 +0100

    feat: Go back to the old style of having a node for each word 🎉

--- lib/src/inline/mod.rs
+++ lib/src/inline/mod.rs
@@ -1,7 +1,6 @@
 use std::collections::LinkedList;
 
 use pandoc_ast::Inline;
-use regex::Regex;
 
 use crate::Meta;
 
@@ -11,54 +10,29 @@ use crate::Meta;
 ///
 /// LinkedList is used because concatenating two lists *should* be cheap in theory
 /// TODO benchmarking
-pub(super) fn parse(meta: &mut Meta, mut range: (usize, usize)) -> LinkedList<Inline> {
-    let whitespace_pattern = Regex::new(r"\s+").unwrap();
 
+pub(super) fn parse(meta: &mut Meta) -> LinkedList<Inline> {
     let node = meta.tree.node();
 
-    let mut inlines = if node.range().start_byte > range.0 {
-        let mut inlines = LinkedList::new();
+    let mut inlines = match node.kind() {
+        "paragraph_break" | "soft_break" => LinkedList::from([Inline::SoftBreak]),
+        "whitespace" => LinkedList::from([Inline::Space]),
 
-        let segment = String::from_utf8_lossy(&meta.source[range.0..node.start_byte()]);
-        range.0 = node.end_byte();
+        "word" | "punctuation" => {
+            LinkedList::from([Inline::Str(node.utf8_text(meta.source).unwrap().to_owned())])
+        }
 
-        whitespace_pattern.split(&segment).for_each(|word| {
-            if !word.is_empty() {
-                inlines.push_back(Inline::Str(word.to_string()));
-                inlines.push_back(Inline::Space);
-            }
-        });
-        inlines.pop_back();
-
-        inlines
-    } else {
-        LinkedList::new()
-    };
-
-    match node.kind() {
-        "\n" => inlines.push_back(Inline::SoftBreak),
 
         _ => {
             eprintln!("{:?} not implemented", node.kind());
-            inlines.push_back(Inline::Str(node.utf8_text(meta.source).unwrap().to_owned()))
+            LinkedList::from([Inline::Str(node.utf8_text(meta.source).unwrap().to_owned())])
         }
-    }
+    };
 
-    if meta.tree.goto_next_sibling() {
-        let mut following = parse(meta, range);
+    if meta.tree.goto_next_sibling() && !meta.tree.node().kind().ends_with("close") {
+        let mut following = parse(meta);
         inlines.append(&mut following);
     } else {
-        if range.0 < range.1 {
-            let segment = String::from_utf8_lossy(&meta.source[range.0..range.1]);
-
-            whitespace_pattern.split(&segment).for_each(|word| {
-                if !word.is_empty() {
-                    inlines.push_back(Inline::Str(word.to_string()));
-                    inlines.push_back(Inline::Space);
-                }
-            });
-            inlines.pop_back();
-        }
         if let Some(back) = inlines.back() {
             if matches!(back, Inline::SoftBreak) {
                 inlines.pop_back();
